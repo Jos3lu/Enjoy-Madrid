e3f416839b93b9dd97a35c69c3218b9418298c6c
Jose Luis <jlsierrabenito@gmail.com>
2021-03-05 00:04:20 +0100

    Modified side menu

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  
+  darkTheme: boolean;
+
+  constructor() {
+    this.activateDarkTheme();
+    
+  }
+
+  activateDarkTheme() {
+    const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+    this.darkTheme = prefersDarkTheme.matches;
+    if (this.darkTheme) 
+      document.body.classList.toggle('dark');
+  }
+
+  toggleTheme() {
+    this.darkTheme = !this.darkTheme;
+    document.body.classList.toggle('dark');
+  }
+
+
 }
