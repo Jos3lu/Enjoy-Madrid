556ebd0da824eef4a28946437675d6773218dad2
Jose Luis <jlsierrabenito@gmail.com>
2021-03-08 21:31:11 +0100

    Updated dark mode

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -7,23 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  // Bound to the value of the dark mode toggle
   darkTheme: boolean;
 
   constructor() {
-    this.activateDarkTheme();
-    
+    this.selectDarkOrLightTheme();
   }
 
-  activateDarkTheme() {
+  selectDarkOrLightTheme() {
+    // Get preference of user about color scheme
     const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
     this.darkTheme = prefersDarkTheme.matches;
-    if (this.darkTheme) 
-      document.body.classList.toggle('dark');
+    this.toggleTheme();
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDarkTheme.addEventListener(
+      'change',
+      mediaQuery => {
+        this.darkTheme = mediaQuery.matches;
+        this.toggleTheme();
+      }
+    );
+  }
+
+  updateTheme() {
+    // Called when dark mode toggle is changed
+    this.darkTheme = !this.darkTheme
+    this.toggleTheme();
   }
 
   toggleTheme() {
-    this.darkTheme = !this.darkTheme;
-    document.body.classList.toggle('dark');
+    // Toggle the dark class on the <body>
+    document.body.classList.toggle('dark', this.darkTheme);
   }
 
 
